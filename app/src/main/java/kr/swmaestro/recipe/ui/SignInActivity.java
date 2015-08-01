bea94a215293c8ea5b39a0cd052234a5d47a0493
가혜 <dnlrkgp@gmail.com>
2015-08-01 19:36:39 +0900

    Set effect blur to background image / make effect alpha color

--- app/src/main/java/kr/swmaestro/recipe/ui/SignInActivity.java
+++ app/src/main/java/kr/swmaestro/recipe/ui/SignInActivity.java
@@ -2,13 +2,18 @@ package kr.swmaestro.recipe.ui;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import kr.swmaestro.recipe.R;
+import kr.swmaestro.recipe.helper.MakeBlurHelper;
 
 /**
  * Created by lk on 2015. 7. 31..
@@ -25,12 +30,26 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void initView() {
+        ImageView bgImageView = (ImageView) findViewById(R.id.activity_singnin_background);
+        Bitmap blurImage = MakeBlurHelper.makeBlur(getApplicationContext(), getBitmapFromDrawable(), 5);
+        bgImageView.setImageBitmap(blurImage);
         final EditText emailEt = (EditText) findViewById(R.id.et_signin_email);
         final EditText passwordEt = (EditText) findViewById(R.id.et_signin_password);
         final Button signinBt = (Button) findViewById(R.id.bt_signin_signin);
         final Button signupBt = (Button) findViewById(R.id.bt_signin_signup);
         signinBt.setOnClickListener(this);
         signupBt.setOnClickListener(this);
+
+
+    }
+
+    private Bitmap getBitmapFromDrawable() {
+        BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(R.drawable.test);
+        if (drawable != null) {
+            Bitmap bitmap = drawable.getBitmap();
+            return bitmap;
+        }
+        return null;
     }
 
 
